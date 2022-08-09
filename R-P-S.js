074c19021c3b7c324c36c8613c94dc2f45001b53
DragonBorn4112 <DragonBorn4112@gmail.com>
2022-08-09 17:06:07 +0200

    Changed functions

--- R-P-S.js
+++ R-P-S.js
@@ -1,23 +1,19 @@
-//
+let playerScore = 0;
+let computerScore = 0;
+let choices = ("Rock", "Paper", "Scissors");
 
 function getComputerChoice(){
-
+   return choices = [Math.floor(Math.random()* choices.length)];
 }
 
-//
-
-function playRound(playerSelelction, computerSelection){
-
+function playRound(playerSelection, computerSelection){
+   if((playerSelection == "rock" && computerSelection == "Scissors") || 
+   (playerSelection == "scissors" && computerSelection == "Paper") ||
+   (playerSelection == "paper" && computerSelection == "Rock")){
+   playerScore += 1; result = ("You win!" + playerSelection + "beats" + computerSelection);}
+   return result;
 }
 
-const playerSelelction = ("Rock!");
+const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelelction, computerSelection));
-
-//
-
-function game(){
-    for(i = 0; i <= 5; i++){
-        
-    }
-}
+console.log(playRound(playerSelection, computerSelection));
