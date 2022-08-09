948e3f5f9dce4f8e52105c34b944420d3ec82821
DragonBorn4112 <DragonBorn4112@gmail.com>
2022-08-09 12:35:30 +0200

    Created Functions

---
+++ R-P-S.js
@@ -0,0 +1,23 @@
+//
+
+function getComputerChoice(){
+
+}
+
+//
+
+function playRound(playerSelelction, computerSelection){
+
+}
+
+const playerSelelction = ("Rock!");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelelction, computerSelection));
+
+//
+
+function game(){
+    for(i = 0; i <= 5; i++){
+        
+    }
+}
